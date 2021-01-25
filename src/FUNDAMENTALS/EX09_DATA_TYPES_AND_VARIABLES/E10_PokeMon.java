3de7a8ec6b5c104ad6ac1092d4d2a2cb9d3756ea
TRU <tedyuzunova@gmail.com>
2021-01-25 17:26:31 +0200

    E10 E11
    Add E10, E11

--- src/FUNDAMENTALS/EX09_DATA_TYPES_AND_VARIABLES/E10_PokeMon.java
+++ src/FUNDAMENTALS/EX09_DATA_TYPES_AND_VARIABLES/E10_PokeMon.java
@@ -6,6 +6,24 @@ public class E10_PokeMon {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int pokePower = Integer.parseInt(scanner.nextLine());
+        int distance = Integer.parseInt(scanner.nextLine());
+        int exhaustedFactor = Integer.parseInt(scanner.nextLine());
+        double percentage = (double) pokePower / 2;
 
+        int count = 0;
+
+        while (pokePower >= distance) {
+            pokePower -= distance;
+            count++;
+
+            if (pokePower == percentage) {
+                if (exhaustedFactor > 0) {
+                    pokePower /= exhaustedFactor;
+                }
+            }
+        }
+        System.out.println(pokePower);
+        System.out.println(count);
     }
 }
