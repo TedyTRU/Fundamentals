cb4cdb6c9877cef403b90c57dbc05bcfa128a365
TRU <tedyuzunova@gmail.com>
2021-02-01 17:12:01 +0200

    Create LAB14_METHODS
    Create L01

---
+++ src/FUNDAMENTALS/LAB14_METHODS/L01_SignOfIntegerNumbers.java
@@ -0,0 +1,11 @@
+package FUNDAMENTALS.LAB14_METHODS;
+
+import java.util.Scanner;
+
+public class L01_SignOfIntegerNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
