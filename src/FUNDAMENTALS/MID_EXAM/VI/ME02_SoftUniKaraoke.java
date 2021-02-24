68b7b40b36545f6bb234f3f242adcbcb23c957f5
TRU <tedyuzunova@gmail.com>
2021-02-24 16:45:20 +0200

    EXAM_IV

---
+++ src/FUNDAMENTALS/MID_EXAM/VI/ME02_SoftUniKaraoke.java
@@ -0,0 +1,35 @@
+package FUNDAMENTALS.MID_EXAM.VI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ME02_SoftUniKaraoke {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> participants = Arrays.stream(scanner.nextLine().split(", ")).collect(Collectors.toList());
+        List<String> songs = Arrays.stream(scanner.nextLine().split(", ")).collect(Collectors.toList());
+        String command = scanner.nextLine();
+        List<String> awards = new ArrayList<>();
+
+        while (!"dawn".equals(command)) {
+            String[] performance = command.split(", ");
+            String participant = performance[0];
+            String song = performance[1];
+            String award = performance[2];
+
+            if (participants.contains(participant) && songs.contains(song)) {
+
+
+            }
+
+
+            command = scanner.nextLine();
+        }
+
+
+    }
+}
