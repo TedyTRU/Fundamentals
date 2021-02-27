3750224f3e01a5c1a5faf47a16ddebcaa458376c
Teodora Uzunova <77300101+TedyTRU@users.noreply.github.com>
2021-02-27 12:00:34 +0200

    ADD MID_EXAM_VII

---
+++ src/FUNDAMENTALS/MID_EXAM/V/ME01_BonusScoringSystem.java
@@ -0,0 +1,25 @@
+package FUNDAMENTALS.MID_EXAM.V;
+
+import java.util.Scanner;
+
+public class ME01_BonusScoringSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int countOfStudents = Integer.parseInt(scanner.nextLine());
+        int countOfLectures = Integer.parseInt(scanner.nextLine());
+        int initialBonus = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < countOfStudents; i++) {
+            int attendances = Integer.parseInt(scanner.nextLine());
+
+
+
+        }
+
+
+
+
+
+    }
+}
