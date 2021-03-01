871fa6060fcd51c5d68febcd16451794304df80a
Teodora Uzunova <77300101+TedyTRU@users.noreply.github.com>
2021-03-01 10:35:51 +0200

    Update ME01

--- src/FUNDAMENTALS/MID_EXAM/V/ME01_BonusScoringSystem.java
+++ src/FUNDAMENTALS/MID_EXAM/V/ME01_BonusScoringSystem.java
@@ -9,17 +9,22 @@ public class ME01_BonusScoringSystem {
         int countOfStudents = Integer.parseInt(scanner.nextLine());
         int countOfLectures = Integer.parseInt(scanner.nextLine());
         int initialBonus = Integer.parseInt(scanner.nextLine());
+        double maxBonus = 0;
+        int studentAttendances = 0;
 
         for (int i = 0; i < countOfStudents; i++) {
             int attendances = Integer.parseInt(scanner.nextLine());
+            double bonus = Math.ceil((double) attendances / countOfLectures * (5 + initialBonus));
 
-
+            if (bonus >= maxBonus) {
+                maxBonus = bonus;
+                studentAttendances = attendances;
+            }
 
         }
 
-
-
-
+        System.out.printf("Max Bonus: %.0f.%n", maxBonus);
+        System.out.printf("The student has attended %d lectures.", studentAttendances);
 
     }
 }
