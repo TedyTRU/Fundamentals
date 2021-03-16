f62ecd9e3429ad1406af650330d2d078e1a091d3
TRU <tedyuzunova@gmail.com>
2021-03-16 09:50:51 +0200

    E03

---
+++ src/FUNDAMENTALS/EX28_TEXT_PROCESSING/E03_ExtractFile_2.java
@@ -0,0 +1,19 @@
+package FUNDAMENTALS.EX28_TEXT_PROCESSING;
+
+import java.util.Scanner;
+
+public class E03_ExtractFile_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String path = scanner.nextLine();
+        String[] pathParts = path.split("\\\\");
+        String fullFileName = pathParts[pathParts.length - 1];
+        String fileName = fullFileName.split("\\.")[0];
+        String extension = fullFileName.split("\\.")[1];
+
+        System.out.println("File name: " + fileName);
+        System.out.println("File extension: " + extension);
+
+    }
+}
