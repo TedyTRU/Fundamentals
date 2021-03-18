6781d173edd8584ac3aea1c0fb408831423d8ca1
TRU <tedyuzunova@gmail.com>
2021-03-18 11:33:38 +0200

    correct FE01

--- src/FUNDAMENTALS/FINAL_EXAM/IV/FE01_PasswordReset.java
+++ src/FUNDAMENTALS/FINAL_EXAM/IV/FE01_PasswordReset.java
@@ -6,39 +6,39 @@ public class FE01_PasswordReset {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String predefinedString = scanner.nextLine();
+        String password = scanner.nextLine();
         String line = scanner.nextLine();
-        StringBuilder rawPassword = new StringBuilder();
 
         while (!"Done".equals(line)) {
+            StringBuilder rawPassword = new StringBuilder();
             String command = line.split(" ")[0];
-            String password;
 
             if ("TakeOdd".equals(command)) {
-                for (int i = 1; i < predefinedString.length(); i += 2) {
-                    char symbol = predefinedString.charAt(i);
+                for (int i = 1; i < password.length(); i += 2) {
+                    char symbol = password.charAt(i);
                     rawPassword.append(symbol);
                 }
-                System.out.println(rawPassword);
+                password = rawPassword.toString();
+                System.out.println(password);
 
             } else if ("Cut".equals(command)) {
                 int index = Integer.parseInt(line.split(" ")[1]);
                 int length = Integer.parseInt(line.split(" ")[2]);
                 int endIndex = index + length;
 
+                rawPassword.append(password);
                 rawPassword.delete(index, endIndex);
-                System.out.println(rawPassword);
+                password = rawPassword.toString();
+
+                System.out.println(password);
 
             } else if ("Substitute".equals(command)) {
                 String substring = line.split(" ")[1];
                 String substitute = line.split(" ")[2];
 
-                if (rawPassword.toString().contains(substring)) {
-                    String currentPass = rawPassword.toString();
-                    currentPass = currentPass.replace(substring, substitute);
-                    rawPassword.delete(0, rawPassword.length());
-                    rawPassword.append(currentPass);
-                    System.out.println(rawPassword.toString());
+                if (password.contains(substring)) {
+                    password = password.replace(substring, substitute);
+                    System.out.println(password);
 
                 } else {
                     System.out.println("Nothing to replace!");
@@ -48,6 +48,6 @@ public class FE01_PasswordReset {
             line = scanner.nextLine();
         }
 
-        System.out.printf("Your password is: %s", rawPassword.toString());
+        System.out.printf("Your password is: %s", password);
     }
 }
