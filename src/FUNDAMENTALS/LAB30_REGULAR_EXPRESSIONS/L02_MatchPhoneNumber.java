d9a66211e25736cfcfa923f14ba55d6b2311d139
TRU <tedyuzunova@gmail.com>
2021-03-19 14:32:04 +0200

    ADD LAB30

---
+++ src/FUNDAMENTALS/LAB30_REGULAR_EXPRESSIONS/L02_MatchPhoneNumber.java
@@ -0,0 +1,30 @@
+package FUNDAMENTALS.LAB30_REGULAR_EXPRESSIONS;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class L02_MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "\\+359([- ])2\\1\\d{3}\\1\\d{4}\\b";
+        String phoneNumbers = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phoneNumbers);
+
+        List<String> matchedPhones = new ArrayList<>();
+
+        while (matcher.find()) {
+            matchedPhones.add(matcher.group());
+
+        }
+
+        System.out.println(String.join(", ", matchedPhones));
+        //System.out.println(matchedPhones.toString().replaceAll("[\\]\\[]", ""));
+
+    }
+}
