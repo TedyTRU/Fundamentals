02d5ccc3b041e33e3ad0a2903db6e4bcecad3d2d
TRU <tedyuzunova@gmail.com>
2021-03-19 15:10:59 +0200

    Create E01

---
+++ src/FUNDAMENTALS/EX31_REGULAR_EXPRESSIONS/E01_Furniture.java
@@ -0,0 +1,43 @@
+package FUNDAMENTALS.EX31_REGULAR_EXPRESSIONS;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class E01_Furniture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String lines = scanner.nextLine();
+        String regex = ">>(?<furniture>[A-Za-z]+)<<(?<price>\\d+[.]*\\d+)!(?<quantity>\\d+)\\b";
+        List<String> listOfFurniture = new ArrayList<>();
+        double finalPrice = 0.0;
+
+
+        while (!"Purchase".equals(lines)) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(lines);
+
+            if (matcher.find()) {
+                String furniture = matcher.group("furniture");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+
+                double totalPrice = price * quantity;
+                finalPrice += totalPrice;
+
+                listOfFurniture.add(furniture);
+
+            }
+
+            lines = scanner.nextLine();
+        }
+
+        System.out.println("Bought furniture:");
+
+        System.out.println(String.join("\n", listOfFurniture));
+
+        System.out.printf("Total money spend: %.2f", finalPrice);
+
+    }
+}
