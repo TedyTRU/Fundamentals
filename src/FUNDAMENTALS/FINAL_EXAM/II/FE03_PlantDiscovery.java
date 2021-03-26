676045c412b5abe548b3bd14a7c8b33470e19dd2
TRU <tedyuzunova@gmail.com>
2021-03-26 16:50:30 +0200

    add FE03(50/50)

--- src/FUNDAMENTALS/FINAL_EXAM/II/FE03_PlantDiscovery.java
+++ src/FUNDAMENTALS/FINAL_EXAM/II/FE03_PlantDiscovery.java
@@ -1,12 +1,66 @@
 package FUNDAMENTALS.FINAL_EXAM.II;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class FE03_PlantDiscovery {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int numberOfLines = Integer.parseInt(scanner.nextLine());
+        Map<String, List<Double>> plants = new HashMap<>();
+        Map<String, List<Double>> plantsRating = new HashMap<>();
 
+        for (int i = 0; i < numberOfLines; i++) {
+            String[] plantInformation = scanner.nextLine().split("<->");
+            String plant = plantInformation[0];
+            int rarity = Integer.parseInt(plantInformation[1]);
 
+            if (!plantsRating.containsKey(plant)) {
+                plantsRating.put(plant, new ArrayList<>());
+            }
+            if (!plants.containsKey(plant)) {
+                plants.put(plant, Arrays.asList(0.0, 0.0));
+            }
+
+            double currentRarity = plants.get(plant).get(0) + rarity;
+            plants.get(plant).set(0, currentRarity);
+        }
+
+        String commands = scanner.nextLine();
+        while (!"Exhibition".equals(commands)) {
+            String command = commands.split(":")[0];
+            String plant = commands.split(":")[1].split(" - ")[0].trim();
+
+            if ("Rate".equals(command)) {
+                double rating = Double.parseDouble(commands.split(":")[1].split(" - ")[1]);
+
+                plantsRating.get(plant).add(rating);
+
+            } else if ("Update".equals(command)) {
+                double rarity = Double.parseDouble(commands.split(":")[1].split(" - ")[1]);
+
+                plants.get(plant).set(0, rarity);
+
+            } else if ("Reset".equals(command)) {
+
+                plantsRating.get(plant).clear();
+                //plants.get(plant).set(1, 0.0);
+
+            } else {
+                System.out.println("error");
+            }
+
+            commands = scanner.nextLine();
+        }
+
+        System.out.println("Plants for the exhibition:");
+
+        plants.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().get(1).compareTo(e1.getValue().get(1)))
+                .sorted((n1, n2) -> n2.getValue().get(0).compareTo(n1.getValue().get(0)))
+                .forEach(plant -> System.out.printf("- %s; Rarity: %.0f; Rating: %.2f%n", plant.getKey(), plant.getValue().get(0),
+                        plantsRating.get(plant.getKey())
+                                .stream().mapToDouble(Double::doubleValue)
+                                .average().orElse(0.0)));
     }
 }
--- src/FUNDAMENTALS/FINAL_EXAM/test.java
+++ src/FUNDAMENTALS/FINAL_EXAM/test.java
@@ -6,19 +6,46 @@ public class test {
     public static void main(String[] args) {
 
 
-        String line = "Port Royal||320000||1000 ";
+//        String line = "Port Royal||320000||1000 ";
+//
+//        //String[] lineOfCities = line.split("\\|+");
+//        String[] lineOfCities = line.trim().split("\\|+");
+//
+//
+//        String city = lineOfCities[0];
+//        int population = Integer.parseInt(lineOfCities[1]);
+//        int gold = Integer.parseInt(lineOfCities[2]);
+//
+//        System.out.println(city);
+//        System.out.println(population);
+//        System.out.println(gold);
+//
+//
+//
+//        String text = "Pesho\\Gosho\\Tosho\\Sasho";
+//        Arrays.stream(text.split("\\\\")).forEach(System.out::println);
 
-        //String[] lineOfCities = line.split("\\|+");
-        String[] lineOfCities = line.trim().split("\\|+");
+
+        Map<String, String> stringMap = new HashMap<>();
+        stringMap.put("Name", "Aman");
+        stringMap.put("Address", "Kolkata");
+
+        System.out.println("Map: " + stringMap);
+
+        stringMap.compute("Name", (key, val) -> val.concat(" Singh"));
+        stringMap.compute("Address", (key, val) -> val.concat(" West-Bengal"));
+
+        System.out.println("New Map: " + stringMap);
 
 
-        String city = lineOfCities[0];
-        int population = Integer.parseInt(lineOfCities[1]);
-        int gold = Integer.parseInt(lineOfCities[2]);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Key1", 12);
+        map.put("Key2", 15);
 
-        System.out.println(city);
-        System.out.println(population);
-        System.out.println(gold);
+        System.out.println("Map: " + map);
+
+        map.compute(null, (key, value) -> value + 3);
+        System.out.println("New Map: " + map);
 
 
     }
